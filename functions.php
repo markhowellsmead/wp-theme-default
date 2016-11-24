15c8443ed5404b3a2301793d5d8ecc230f83e6a6
Mark Howells-Mead <permanent.tourist@gmail.com>
2016-11-24 16:58:36 +0100

    Add Class autoloader

--- functions.php
+++ functions.php
@@ -10,7 +10,15 @@
 if (version_compare($GLOBALS['wp_version'], '4.6-alpha', '<')) {
     require get_template_directory().'/Classes/Utilities/BackCompat.php';
 } else {
-    require_once 'Classes/Theme.php';
+    spl_autoload_register(function ($class_name) {
+        if (false !== strpos($class_name, 'WordpressTheme')) {
+            $classes_dir = realpath(plugin_dir_path(__FILE__)).DIRECTORY_SEPARATOR.'Classes'.DIRECTORY_SEPARATOR;
+            $class_file = str_replace('MHM\WordpressTheme\\', DIRECTORY_SEPARATOR, $class_name).'.php';
+            require_once $classes_dir.$class_file;
+        }
+    });
+
+    new MHM\WordpressTheme\Theme();
 
     /**
      * $content_width is an icky WordPress requirement, which constrains images and oEmbeds
