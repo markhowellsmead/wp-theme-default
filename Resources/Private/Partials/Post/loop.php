7e9c7e0cd3f3da6a68e8f2a2d4a0de30337a0600
Mark Howells-Mead <permanent.tourist@gmail.com>
2016-11-25 13:09:16 +0100

    Lower case file names
    Because they aren’t class files, just regular includes.

--- Resources/Private/Partials/Post/loop.php
+++ Resources/Private/Partials/Post/loop.php
@@ -3,7 +3,7 @@
 if (have_posts()) {
     while (have_posts()) {
         the_post();
-        get_template_part('Resources/Private/Partials/Post/Content', 'loop');
+        get_template_part('Resources/Private/Partials/Post/Content', 'loop'); // Falls back to content.php if content-loop.php isn't available
     }
 } else {
     get_template_part('Resources/Private/Partials/Post/Content', 'none');
