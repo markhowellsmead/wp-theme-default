57bf29cd84a4c18f65661e77c85b8fb0a9b7fbad
Mark Howells-Mead <permanent.tourist@gmail.com>
2016-12-12 13:07:28 +0100

    Add listheader author template part

---
+++ resources/private/partials/listheader-author.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <header>
+        <h1 class="list-title"><?php
+        printf(
+            __('Articles by %1$s', 'TEXT_DOMAIN'),
+            get_the_author_link()
+        );
+        ?></h1>
+    </header>
+</div>
